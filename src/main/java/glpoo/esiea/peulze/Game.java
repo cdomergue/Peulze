1781b71f76e771fe0a4b55ade8426a2c6c175818
cdomergue <christophe.domergue@zenika.com>
2015-03-25 14:46:09 +0100

    Creating maven project

---
+++ src/main/java/glpoo/esiea/peulze/Game.java
@@ -0,0 +1,7 @@
+package glpoo.esiea.peulze.Game;
+
+/**
+ * Created by christophe on 25/03/15.
+ */
+public class Game {
+}
---
+++ src/test/java/glpoo/esiea/tests/GameTest.java
@@ -0,0 +1,7 @@
+package glpoo.esiea.tests;
+
+/**
+ * Created by christophe on 25/03/15.
+ */
+public class GameTest {
+}
